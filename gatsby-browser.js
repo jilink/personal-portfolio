90579294b7c60d924fd5fbb30a4017c0f1b31ccd
jilink <jimmyfac1@gmail.com>
2021-02-07 17:31:37 +0100

    Theme Toggle working

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import { wrapPageElement as wrap } from "./src/woot-wrapper"
+export const wrapPageElement = wrap
